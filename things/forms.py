e976c2e3b6fc866a86fe389f99652719a2d5897b
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2023-12-04 21:09:56 +0000

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,17 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    def clean(self):
+        # Optional: Add custom validation logic if needed
+        cleaned_data = super().clean()
+        # Example: Validate that the quantity is a positive number
+        quantity = cleaned_data.get("quantity")
+        if quantity is not None and quantity < 1:
+            self.add_error('quantity', 'Quantity must be a positive number')
+        return cleaned_data
+
