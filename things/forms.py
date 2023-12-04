49b69949cce9cd5a9df19fe80fb945b17fe02130
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2023-12-04 21:10:40 +0000

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -7,9 +7,7 @@ class ThingForm(forms.ModelForm):
         fields = ['name', 'description', 'quantity']
 
     def clean(self):
-        # Optional: Add custom validation logic if needed
         cleaned_data = super().clean()
-        # Example: Validate that the quantity is a positive number
         quantity = cleaned_data.get("quantity")
         if quantity is not None and quantity < 1:
             self.add_error('quantity', 'Quantity must be a positive number')
