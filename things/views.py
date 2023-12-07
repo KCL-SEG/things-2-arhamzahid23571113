c2f30a5151028d144c5abf2bb87c7567013aa33a
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2023-12-07 00:33:22 +0000

    Update views.py

--- things/views.py
+++ things/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .forms import ThingForm  
+from .forms import ThingForm
 
 def home(request):
-    form = ThingForm() 
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            # For example, you can save the form
+            form.save()
+            # Redirect or indicate success after saving
+    else:
+        form = ThingForm()  # An unbound form
+
     return render(request, 'home.html', {'form': form})
