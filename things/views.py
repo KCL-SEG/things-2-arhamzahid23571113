d63050b465ea80fd59b2d8b1d00669db1609ea86
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2023-12-07 00:34:57 +0000

    Update views.py

--- things/views.py
+++ things/views.py
@@ -5,10 +5,9 @@ def home(request):
     if request.method == 'POST':
         form = ThingForm(request.POST)
         if form.is_valid():
-            # Process the data in form.cleaned_data
-            # For example, you can save the form
+            
             form.save()
-            # Redirect or indicate success after saving
+            
     else:
         form = ThingForm()  # An unbound form
 
