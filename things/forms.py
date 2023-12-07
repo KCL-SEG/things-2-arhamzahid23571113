db2752ab57f85f6551f2209719489eb90309e9e4
arhamzahid23571113 <146372787+arhamzahid23571113@users.noreply.github.com>
2023-12-07 00:33:00 +0000

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -5,11 +5,7 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        quantity = cleaned_data.get("quantity")
-        if quantity is not None and quantity < 1:
-            self.add_error('quantity', 'Quantity must be a positive number')
-        return cleaned_data
-
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 40, 'rows': 5}),
+            'quantity': forms.NumberInput()
+        }
